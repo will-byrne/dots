3a062810f5e19d121c31d5b6cd43777fe5dcc2db
Will Byrne <will.alex.byrne@gmail.com>
2025-05-04 12:47:13 +0100

    more waybar config

--- hosts/p14/default.nix
+++ hosts/p14/default.nix
@@ -11,6 +11,8 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+  services.blueman.enable = true;
+
   
   networking.hostName = "p14nixos";
 }
