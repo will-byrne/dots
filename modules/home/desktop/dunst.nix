5b9f7bc494c45ac637d6ebeb05afb7b964175fbf
Will Byrne <will.alex.byrne@gmail.com>
2025-06-19 22:35:41 +0100

    update dunst

--- modules/home/desktop/dunst.nix
+++ modules/home/desktop/dunst.nix
@@ -15,31 +15,35 @@
         progress_bar_corner_radius = 5;
         icon_corner_radius = 5;
         corner_radius = 5;
-        frame_width = 0;
+        frame_width = 2;
         gap_size = 1;
         font = "Iosevka Nerd Font 9";
         format = "<b>%s</b>\n%b";
         enable_recursive_icon_lookup = true;
         min_icon_size = 32;
         max_icon_size = 64;
+        frame_color = "#8aadf4";
+        separator_color= "frame";
+        highlight = "#8aadf4";
+        transparency = 5;
       };
 
       urgency_low = {
-        background = "#16161e";
-        foreground = "#c0caf5";
-        frame_color = "#c0caf5";
+        background = "#24273a";
+        foreground = "#cad3f5";
+        # frame_color = "#c0caf5";
       };
 
       urgency_normal = {
-        background = "#1a1b26";
-        foreground = "#c0caf5";
-        frame_color = "#c0caf5";
+        background = "#24273a";
+        foreground = "#cad3f5";
+        # frame_color = "#c0caf5";
       };
 
       urgency_critical = {
-        background = "#292e42";
-        foreground = "#db4b4b";
-        frame_color = "#db4b4b";
+        background = "#24273a";
+        foreground = "#cad3f5";
+        frame_color = "#f5a97f";
       };
     };
   };
