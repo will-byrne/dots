d16e1752132ac8ae6cb139192cd5e9adce4e1c25
Will Byrne <will.alex.byrne@gmail.com>
2025-08-14 14:26:52 +0100

    re-add protonmail-bridge

--- modules/home/programs/email.nix
+++ modules/home/programs/email.nix
@@ -5,27 +5,27 @@
     profiles."default".isDefault = true;
   };
 
-  # systemd.user.services = {
-  #   protonmail-bridge = {
-  #     Unit = {
-  #       Description = "Proton Mail Bridge";
-  #       After = [
-  #         "network.target"
-  #         "gnome-keyring.service"
-  #       ];
-  #     };
-  #     Service = {
-  #       Type = "simple";
-  #       ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
-  #       Restart = "on-failure";
-  #       RestartSec = 5;
-  #       Environment = "PATH=${pkgs.gnome-keyring}/bin:$PATH";
-  #     };
-  #     Install = {
-  #       WantedBy = [ "default.target" ];
-  #     };
-  #   };
-  # };
+  systemd.user.services = {
+    protonmail-bridge = {
+      Unit = {
+        Description = "Proton Mail Bridge";
+        After = [
+          "network.target"
+          "gnome-keyring.service"
+        ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --noninteractive";
+        Restart = "on-failure";
+        RestartSec = 5;
+        Environment = "PATH=${pkgs.gnome-keyring}/bin:$PATH";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
 
   home.packages = with pkgs; [
     protonmail-bridge
