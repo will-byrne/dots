d73869dfb1112a02d4de98e1cb4fb1c9bc48748a
Will Byrne <will.alex.byrne@gmail.com>
2025-08-18 20:03:46 +0100

    update flake

--- modules/home/desktop/hyprland/hyprpaper.nix
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -5,27 +5,27 @@
   hostname,
   ...
 }: let
-  wallpaper =if hostname == "p14" then
-    pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/misc/rainbow-cat.png";
-      hash = "sha256-WP+kQ7mgjpeekatDEPSP/XeDc5ZihCm+BxgqgwYDIEU=";
-    }
-  else
-    pkgs.fetchurl {
-      url = "https://www.alexotos.com/wp-content/uploads/2020/03/4k-wallpaper.jpg";
-      hash = "sha256-3KV5+Ig1e7TfdJxUYWvltczRgN8O7bpOFGKbx7SKbqI=";
-    };
+  wallpaperCat = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/misc/rainbow-cat.png";
+    hash = "sha256-WP+kQ7mgjpeekatDEPSP/XeDc5ZihCm+BxgqgwYDIEU=";
+  };
+  
+  wallpaperSwitch = pkgs.fetchurl {
+    url = "https://www.alexotos.com/wp-content/uploads/2020/03/4k-wallpaper.jpg";
+    hash = "sha256-3KV5+Ig1e7TfdJxUYWvltczRgN8O7bpOFGKbx7SKbqI=";
+  };
 in {
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       preload = [
-        (builtins.toString wallpaper)
+        (builtins.toString wallpaperCat)
+        (builtins.toString wallpaperSwitch)
       ];
 
       wallpaper = [
-        ",${builtins.toString wallpaper}"
+        ",${builtins.toString wallpaperSwitch}"
       ];
     };
   };
