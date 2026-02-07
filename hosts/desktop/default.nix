ba96a55c1e4fbaaec66fc96ed992e3f5e07d1066
Will Byrne <will.alex.byrne@gmail.com>
2026-02-07 19:42:02 +0000

    update desktop config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,16 +1,50 @@
-{ hostname, lib, ... }:
+{ hostname, lib, pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ../optional/amd.nix
   ];
-  services.xserver.videoDrivers = lib.mkDefault [ "modesetting" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Needed so users can read hwmon without sudo
+  services.udev.extraRules = ''
+    KERNEL=="hwmon*", MODE="0644"
+  '';
 
   hardware.graphics = {
-    enable = lib.mkDefault true;
-    enable32Bit = lib.mkDefault true;
+    enable = true;
+    enable32Bit = true;
+
+    extraPackages = with pkgs; [
+      mesa
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-tools
+    ];
+
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      mesa
+      vulkan-loader
+    ];
   };
 
-  hardware.amdgpu.initrd.enable = lib.mkDefault true;
+  hardware.amdgpu.opencl.enable = true;
+
+  systemd.tmpfiles.rules = 
+  let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        rocm-smi
+        hipblas
+        clr
+      ];
+    };
+  in [
+    "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+  ];
+
+  hardware.amdgpu.initrd.enable = true;
   networking.hostName = hostname;
 }
