3def65ac84d4ac9083bcc313850eeef77ace3f7f
Will Byrne <will.alex.byrne@gmail.com>
2026-02-07 18:29:27 +0000

    update desktop config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,9 +1,16 @@
-{ hostname, ... }:
+{ hostname, lib, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ../optional/amd.nix
   ];
-  
+  services.xserver.videoDrivers = lib.mkDefault [ "modesetting" ];
+
+  hardware.graphics = {
+    enable = lib.mkDefault true;
+    enable32Bit = lib.mkDefault true;
+  };
+
+  hardware.amdgpu.initrd.enable = lib.mkDefault true;
   networking.hostName = hostname;
 }
