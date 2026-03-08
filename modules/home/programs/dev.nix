a660d3f7760462152dd58d93544e06f0f8e10421
Will Byrne <will.alex.byrne@gmail.com>
2026-03-08 23:01:29 +0000

    replace lunarvim as it is no longer maintained with astronvim

--- modules/home/programs/dev.nix
+++ modules/home/programs/dev.nix
@@ -5,21 +5,9 @@
     jetbrains.idea-oss
     jetbrains.rust-rover
     jetbrains.webstorm
-    lunarvim
     neovide
     zed-editor-fhs
     vscode
     postman
   ];
-
-  # add custom desktop entry for running LunarVim inside Neovide
-  xdg.desktopEntries.neoluna = {
-    name = "NeoLuna";
-    genericName = "Text Editor";
-    comment = "Launch LunarVim in Neovide";
-    exec = "neovide -- -u ${pkgs.lunarvim}/share/lvim/init.lua";
-    icon = "lvim";
-    terminal = false;
-    categories = [ "Utility" "TextEditor" ];
-  };
 }
--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -1,16 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.neovim = {
-    enable = true;
+    defaultEditor = true;
+    withNodeJs = true;
+    withPython3 = true;
+    plugins = with pkgs.vimPlugins; [
+      astroui
+      astrocore
+      astrolsp
+      astrotheme
+    ];
   };
-
-  xdg.configFile = {
-    "nvim/init.lua".source = ./config/init.lua;
-    "nvim/lua".source = ./config/lua;
+  home.file = {
+    ".config/nvim".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dots/modules/home/programs/neovim/nvim";
   };
-
   # ls, fmt, linters
   home.packages = with pkgs; [
+    nodejs_25
+    neovim
+    gcc
+    gnumake
+    unzip
+    tree-sitter
+    bottom
+    gdu
+    fd
+    ripgrep
+    lazygit
     gopls
     clang-tools
     typescript-language-server
