89a1729abfd7e54096dca4b201c32a1b065c7fe6
Will Byrne <will.alex.byrne@gmail.com>
2026-03-31 13:00:52 +0100

    add zathura catppuccin colors

--- modules/home/programs/zathura.nix
+++ modules/home/programs/zathura.nix
@@ -6,6 +6,36 @@
     };
     options = {
       selection-clipboard = "clipboard";
+      default-fg = "rgba(202,211,245,1)";
+      default-bg = "rgba(36,39,58,1)";
+      completion-bg = "rgba(54,58,79,1)";
+      completion-fg = "rgba(202,211,245,1)";
+      completion-highlight-bg = "rgba(198,160,246,1)";
+      completion-highlight-fg = "rgba(36,39,58,1)";
+      completion-group-bg = "rgba(30,32,48,1)";
+      completion-group-fg = "rgba(202,211,245,1)";
+      statusbar-fg = "rgba(202,211,245,1)";
+      statusbar-bg = "rgba(24,25,38,1)";
+      inputbar-fg = "rgba(202,211,245,1)";
+      inputbar-bg = "rgba(36,39,58,1)";
+      notification-bg = "rgba(36,39,58,1)";
+      notification-fg = "rgba(202,211,245,1)";
+      notification-error-bg = "rgba(36,39,58,1)";
+      notification-error-fg = "rgba(237,135,150,1)";
+      notification-warning-bg = "rgba(36,39,58,1)";
+      notification-warning-fg = "rgba(238,212,159,1)";
+      recolor = "true";
+      recolor-lightcolor = "rgba(36,39,58,1)";
+      recolor-darkcolor = "rgba(202,211,245,1)";
+      index-fg = "rgba(202,211,245,1)";
+      index-bg = "rgba(36,39,58,1)";
+      index-active-fg = "rgba(202,211,245,1)";
+      index-active-bg = "rgba(54,58,79,1)";
+      render-loading-bg = "rgba(36,39,58,1)";
+      render-loading-fg = "rgba(202,211,245,1)";
+      highlight-color = "rgba(147,154,183,0.3)";
+      highlight-fg = "rgba(202,211,245,1)";
+      highlight-active-color = "rgba(198,160,246,0.3)";
     };
   };
 }
