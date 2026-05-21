f9f345026f881f8e720afb0491f612041f2a0aa8
Will Byrne <will.alex.byrne@gmail.com>
2026-05-21 16:31:10 +0100

    fix laptop dots startup command

--- modules/home/desktop/hyprland/lua/autostart.lua
+++ modules/home/desktop/hyprland/lua/autostart.lua
@@ -8,7 +8,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("discord", { workspace = "2" })
     hl.exec_cmd("signal-desktop", { workspace = "2" })
   else
-    hl.exec_cmd("kitty --class dots ~/dots", { workspace = "1" })
+    hl.exec_cmd("kitty --class dots --directory ~/dots nvim", { workspace = "1" })
     hl.exec_cmd("firefox", { workspace = "2" })
     hl.exec_cmd("discord", { workspace = "3" })
     hl.exec_cmd("signal-desktop", { workspace = "3" })
