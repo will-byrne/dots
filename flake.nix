704c7e16061c487797f7a0c123e9ce5588dd2aad
Will Byrne <will.alex.byrne@gmail.com>
2026-05-31 20:37:56 +0100

    update flake and specify home-manager 26.05

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
