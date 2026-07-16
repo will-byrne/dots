7a6e33f6b12e552f22acc536c439de4a54d509f8
Will Byrne <will.alex.byrne@gmail.com>
2026-07-16 16:46:23 +0100

    cleanup commented config

--- modules/home/desktop/gtk.nix
+++ modules/home/desktop/gtk.nix
@@ -13,14 +13,6 @@
     };
     gtk4.enable= false;
   };
-  # xdg.configFile = {
-  #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-  #   "gtk-4.0/gtk.css" = {
-  #     source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-  #     force = true;
-  #   };
-  #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  # };
 
   home.packages = with pkgs; [
     nwg-look
